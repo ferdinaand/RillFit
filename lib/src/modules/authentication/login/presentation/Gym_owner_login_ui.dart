68b7d80585ff5d3e33ca950b5219f94d35e2bc31
ferdinaand <ekpo546@gmail.com>
2023-06-13 23:09:02 +0100

    update

---
+++ lib/src/modules/authentication/login/presentation/Gym_owner_login_ui.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: unnecessary_statements
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:riilfit/src/data/enum/view_state.enum.dart';
+import 'package:riilfit/src/modules/authentication/login/controller/login.controller.dart';
+import 'package:riilfit/src/modules/authentication/login/presentation/widgets/login_form.ui.dart';
+import 'package:riilfit/src/presentation/global_widgets/network_loader.ui.dart';
+import 'package:riilfit/src/presentation/resources/res.dart';
+import 'package:riilfit/src/presentation/widgets.dart';
+
+class GymOwnerLoginUi extends GetView<LoginController> {
+  const GymOwnerLoginUi({super.key});
+
+  @override
+  LoginController get controller => Get.put(LoginController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () {
+        if (controller.viewState.isBusy) {
+          return const NetworkLoaderUi(
+            message: 'Hold on, signing you in...',
+          );
+        }
+        return GestureDetector(
+          onTap: () {
+            final currentFocus = FocusScope.of(context);
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+            }
+          },
+          child: Scaffold(
+            appBar: const AuthAppbarUi(),
+            body: SafeArea(
+              child: Padding(
+                padding: baseViewPadding,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
