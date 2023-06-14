d21bd25a25f8561583739d1129116ee8603bb555
ferdinaand <ekpo546@gmail.com>
2023-06-14 23:38:30 +0100

    update

--- lib/src/modules/dashboard/role/choose_service/widget/Role.Card.ui.dart
+++ lib/src/modules/dashboard/role/choose_service/widget/Role.Card.ui.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:riilfit/src/presentation/global_widgets/text.ui.dart';
 import 'package:riilfit/src/presentation/resources/colors.res.dart';
 
+import '../../../../../presentation/resources/icons.res.dart';
 import '../../../../../presentation/widgets.dart';
 
 // ignore: must_be_immutable
@@ -23,12 +24,12 @@ class RoleCardUi extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(14),
       child: Container(
-        height: 124.h,
-        width: 134.h,
+        height: 140.h,
+        width: 142.h,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           border: Border.all(
-            width: 1,
+            width: 2,
             color: isSelected ? primary : white,
           ),
         ),
@@ -38,11 +39,26 @@ class RoleCardUi extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Visibility(
+                  visible: isSelected,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        roleTickIcon,
+                        height: 15,
+                        width: 15,
+                      ),
+                      const Gap(5)
+                    ],
+                  ),
+                ),
                 Image.asset(image),
                 const Gap(9),
                 TextUi.bodyLarge(
                   role,
                   fontSize: 14,
+                  fontWeight: FontWeight.w400,
                 )
               ],
             ),
