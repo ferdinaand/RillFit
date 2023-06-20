663c415e8b6a866addf00f1d9eaff7fdabc248db
ferdinaand <ekpo546@gmail.com>
2023-06-20 23:51:40 +0100

    update

---
+++ lib/src/modules/dashboard/stores/stores.item.details/presentation/store.item.details.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:riilfit/src/data/dummy/onboarding_carousel.dummy.dart';
+import 'package:riilfit/src/modules/onboarding/controller/onboarding.controller.dart';
+import 'package:riilfit/src/modules/onboarding/presentation/widgets/onboarding_carousel.ui.dart';
+import 'package:riilfit/src/modules/onboarding/presentation/widgets/page_indicator.dart';
+import 'package:riilfit/src/presentation/resources/res.dart';
+import 'package:riilfit/src/presentation/widgets.dart';
+
+class StoreItemController extends GetView<OnboardingController> {
+  const StoreItemController({super.key});
+
+  @override
+  OnboardingController get controller => Get.put(OnboardingController());
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        body: Column(),
+      ),
+    );
+  }
+}
