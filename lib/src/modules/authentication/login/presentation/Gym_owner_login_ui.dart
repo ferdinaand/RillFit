bc1c25d3f37e67f0c034f52fc25986990ace3c50
ferdinaand <ekpo546@gmail.com>
2023-06-30 23:21:32 +0100

    update

--- lib/src/modules/authentication/login/presentation/Gym_owner_login_ui.dart
+++ lib/src/modules/authentication/login/presentation/Gym_owner_login_ui.dart
@@ -1,19 +1,23 @@
 // ignore_for_file: unnecessary_statements
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:riilfit/src/data/enum/view_state.enum.dart';
 import 'package:riilfit/src/modules/authentication/login/controller/login.controller.dart';
+import 'package:riilfit/src/modules/authentication/login/presentation/widgets/Gym_Owner_login_form_ui.dart';
 import 'package:riilfit/src/modules/authentication/login/presentation/widgets/login_form.ui.dart';
 import 'package:riilfit/src/presentation/global_widgets/network_loader.ui.dart';
 import 'package:riilfit/src/presentation/resources/res.dart';
 import 'package:riilfit/src/presentation/widgets.dart';
 
-class GymOwnerLoginUi extends GetView<LoginController> {
+import '../controller/Gym_Owner_login_controller.dart';
+
+class GymOwnerLoginUi extends GetView<GymOwnerLoginController> {
   const GymOwnerLoginUi({super.key});
 
   @override
-  LoginController get controller => Get.put(LoginController());
+  GymOwnerLoginController get controller => Get.put(GymOwnerLoginController());
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +40,32 @@ class GymOwnerLoginUi extends GetView<LoginController> {
             body: SafeArea(
               child: Padding(
                 padding: baseViewPadding,
-                child: Column(children: []),
+                child: CustomScrollView(
+                  slivers: [
+                    SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Gap(50),
+                          TextUi.bodyLarge(
+                            'Gym Owner',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24,
+                          ),
+                          Gap(17),
+                          TextUi.bodyLarge(
+                            'Enter Information about your gym below',
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                          ),
+                          Gap(48),
+                          GymOwnerLoginFormUi(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
