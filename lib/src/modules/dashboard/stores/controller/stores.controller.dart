afe40ba659de03083b56de7d70760a92df07bdec
ferdinaand <ekpo546@gmail.com>
2023-07-05 23:01:49 +0100

    update

--- lib/src/modules/dashboard/stores/controller/stores.controller.dart
+++ lib/src/modules/dashboard/stores/controller/stores.controller.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:riilfit/src/domain/base/controller/base.controller.dart';
+
+import '../../../../utils/config.dart';
 
 class StoresController extends BaseController {
   @override
   void onInit() {
     searchController = TextEditingController();
+    isLoading = true;
+    Future.delayed(const Duration(seconds: 2), () {
+      isLoading = false;
+    });
     super.onInit();
   }
 
