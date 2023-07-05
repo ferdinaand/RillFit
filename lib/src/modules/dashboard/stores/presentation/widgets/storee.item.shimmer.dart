57308fd05346afd7aa14aef6566c5b4872ce0551
ferdinaand <ekpo546@gmail.com>
2023-07-05 23:02:17 +0100

    update

---
+++ lib/src/modules/dashboard/stores/presentation/widgets/storee.item.shimmer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ShimmerSkeleton extends StatelessWidget {
+  const ShimmerSkeleton({
+    required this.height,
+    required this.width,
+    super.key,
+  });
+  final double? height, width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: Colors.grey[700],
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+    );
+  }
+}
