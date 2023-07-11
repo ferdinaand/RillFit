b5dbb29100649a462559d47afabb2bd1961f2258
ferdinaand <ekpo546@gmail.com>
2023-07-11 23:08:12 +0100

    update

--- lib/src/modules/dashboard/gym/gym_locations/presentation/gym_locations.dart
+++ lib/src/modules/dashboard/gym/gym_locations/presentation/gym_locations.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:riilfit/src/modules/dashboard/gym/gym_locations/presentation/widgets/gym_location_card.dart';
 import 'package:riilfit/src/presentation/global_widgets/text.ui.dart';
 import 'package:riilfit/src/presentation/resources/colors.res.dart';
 
@@ -29,6 +30,26 @@ class _SelectLocationState extends State<SelectLocation> {
                 height: 28.h,
               ),
             ),
+            const Gap(50),
+            Row(
+              children: const [
+                TextUi.heading3(
+                  'Available Location',
+                  fontSize: 24,
+                )
+              ],
+            ),
+            Gap(45),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  LocationCard(location: 'ogun'),
+                  LocationCard(location: 'ogun'),
+                  LocationCard(location: 'ogun'),
+                  LocationCard(location: 'ogun'),
+                ],
+              ),
+            )
           ],
         ),
       )),
