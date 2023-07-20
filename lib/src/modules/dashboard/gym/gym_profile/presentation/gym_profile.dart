b51d9d9f0a5bec5f938bc951080225fc7aa71597
ferdinaand <ekpo546@gmail.com>
2023-07-21 00:02:22 +0100

    update

---
+++ lib/src/modules/dashboard/gym/gym_profile/presentation/gym_profile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:riilfit/src/modules/dashboard/gym/gyms_list/controller/gym.controller.dart';
+import 'package:riilfit/src/modules/dashboard/gym/gyms_list/presentation/widgets/gym_card.ui.dart';
+import 'package:riilfit/src/modules/dashboard/gym/gyms_list/presentation/widgets/search_field.ui.dart';
+import 'package:riilfit/src/presentation/resources/res.dart';
+import 'package:riilfit/src/presentation/widgets.dart';
+
+class GymProfile extends GetView<GymController> {
+  const GymProfile({super.key});
+
+  @override
+  GymController get controller => Get.put(GymController());
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        appBar: const MainAppbarUi(
+          title: 'Gym Profile',
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
