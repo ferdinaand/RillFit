6220a1858ea16509141c79f423a235184cba1593
ferdinaand <ekpo546@gmail.com>
2023-07-21 16:59:04 +0100

    update

--- lib/src/modules/dashboard/profile/presentation/profile.ui.dart
+++ lib/src/modules/dashboard/profile/presentation/profile.ui.dart
@@ -23,119 +23,133 @@ class ProfileUi extends GetView<ProfileController> {
           currentFocus.unfocus();
         }
       },
-      child: Scaffold(
-        appBar: MainAppbarUi(
-          title: 'Profile',
-          showBackButton: false,
-          actions: [
-            GestureDetector(
-              onTap: controller.themeService.changeThemeMode,
-              child: Obx(
-                () => AnimatedContainer(
-                  duration: fastDuration,
-                  decoration: BoxDecoration(
-                    color: controller.themeService.isDarkMode
-                        ? grayScale700
-                        : grayScale50,
-                    shape: BoxShape.circle,
-                  ),
-                  padding: const EdgeInsets.all(8),
-                  margin: const EdgeInsets.only(right: 12),
-                  child: SvgPicture.asset(
-                    controller.themeService.isDarkMode ? sunIcon : moonIcon,
-                    color: primary20,
-                    height: 24,
-                    width: 24,
-                  ),
-                ),
+      child: controller.isLoading.value
+          ? Center(
+              child: CircularProgressIndicator(
+                color: primary,
+                backgroundColor: Colors.white,
               ),
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: CustomScrollView(
-            physics: const ClampingScrollPhysics(),
-            slivers: [
-              SliverFillRemaining(
-                hasScrollBody: false,
-                child: Column(
-                  children: [
-                    const Gap(16),
-                    Obx(
-                      () => Container(
-                        height: 102,
-                        width: 102,
+            )
+          : Scaffold(
+              appBar: MainAppbarUi(
+                title: 'Profile',
+                showBackButton: false,
+                actions: [
+                  GestureDetector(
+                    onTap: controller.themeService.changeThemeMode,
+                    child: Obx(
+                      () => AnimatedContainer(
+                        duration: fastDuration,
                         decoration: BoxDecoration(
                           color: controller.themeService.isDarkMode
                               ? grayScale700
                               : grayScale50,
                           shape: BoxShape.circle,
                         ),
-                        padding: const EdgeInsets.all(16),
+                        padding: const EdgeInsets.all(8),
+                        margin: const EdgeInsets.only(right: 12),
                         child: SvgPicture.asset(
-                          imagePlaceholder,
+                          controller.themeService.isDarkMode
+                              ? sunIcon
+                              : moonIcon,
                           color: primary20,
+                          height: 24,
+                          width: 24,
                         ),
                       ),
                     ),
-                    const Gap(8),
-                    TextUi.bodyMed(
-                      controller.currentUser.fullName ?? '',
-                      fontWeight: mediumText,
-                    ),
-                    const Gap(4),
-                    TextUi.bodySmall(
-                      controller.currentUser.phoneNumber ?? '',
-                    ),
-                    const Gap(24),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 12,
-                      ),
-                      margin: baseViewPadding,
-                      decoration: const BoxDecoration(
-                        color: primary,
-                        borderRadius: BorderRadius.all(
-                          regularRadius,
-                        ),
-                      ),
-                      child: Row(
+                  ),
+                ],
+              ),
+              body: SafeArea(
+                child: CustomScrollView(
+                  physics: const ClampingScrollPhysics(),
+                  slivers: [
+                    SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Column(
                         children: [
-                          SvgPicture.asset(
-                            favoriteIcon,
-                            color: grayScale50,
-                          ),
                           const Gap(16),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              mainAxisSize: MainAxisSize.min,
+                          Obx(
+                            () => Container(
+                              height: 102,
+                              width: 102,
+                              decoration: BoxDecoration(
+                                color: controller.themeService.isDarkMode
+                                    ? grayScale700
+                                    : grayScale50,
+                                shape: BoxShape.circle,
+                              ),
+                              padding: const EdgeInsets.all(16),
+                              child: SvgPicture.asset(
+                                imagePlaceholder,
+                                color: primary20,
+                              ),
+                            ),
+                          ),
+                          const Gap(8),
+                          TextUi.bodyMed(
+                            controller.currentUser.fullName ?? '',
+                            fontWeight: mediumText,
+                          ),
+                          const Gap(4),
+                          TextUi.bodySmall(
+                            controller.currentUser.phoneNumber ?? '',
+                          ),
+                          const Gap(24),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 12,
+                            ),
+                            margin: baseViewPadding,
+                            decoration: const BoxDecoration(
+                              color: primary,
+                              borderRadius: BorderRadius.all(
+                                regularRadius,
+                              ),
+                            ),
+                            child: Row(
                               children: [
-                                Text.rich(
-                                  TextSpan(
-                                    text:
-                                        'Do you own a gym or sell gym equipments, or are you a gym trainer? ',
-                                    style: bodySmall.copyWith(
-                                      height: 1.5,
-                                      color: grayScale50,
-                                    ),
+                                SvgPicture.asset(
+                                  favoriteIcon,
+                                  color: grayScale50,
+                                ),
+                                const Gap(16),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    mainAxisSize: MainAxisSize.min,
                                     children: [
-                                      TextSpan(
-                                        text: 'Create a vendor account',
-                                        style: bodySmall.copyWith(
-                                          fontWeight: semiBoldText,
-                                          color: white,
-                                          decoration: TextDecoration.underline,
+                                      Text.rich(
+                                        TextSpan(
+                                          text:
+                                              'Do you own a gym or sell gym equipments, or are you a gym trainer? ',
+                                          style: bodySmall.copyWith(
+                                            height: 1.5,
+                                            color: grayScale50,
+                                          ),
+                                          children: [
+                                            TextSpan(
+                                              text: 'Create a vendor account',
+                                              style: bodySmall.copyWith(
+                                                fontWeight: semiBoldText,
+                                                color: white,
+                                                decoration:
+                                                    TextDecoration.underline,
+                                              ),
+                                              recognizer: TapGestureRecognizer()
+                                                ..onTap = controller
+                                                    .createVendorAccount,
+                                            ),
+                                            const TextSpan(
+                                              text:
+                                                  ' now to put yourself on Riilfit and earn!',
+                                              style: bodySmall,
+                                            ),
+                                          ],
                                         ),
-                                        recognizer: TapGestureRecognizer()
-                                          ..onTap =
-                                              controller.createVendorAccount,
-                                      ),
-                                      const TextSpan(
-                                        text:
-                                            ' now to put yourself on Riilfit and earn!',
-                                        style: bodySmall,
                                       ),
                                     ],
                                   ),
@@ -143,41 +157,38 @@ class ProfileUi extends GetView<ProfileController> {
                               ],
                             ),
                           ),
+                          const Spacer(),
+                          const Gap(16),
+                          const ProfileTileUi(
+                            icon: userIcon,
+                            title: 'Edit profile info',
+                          ),
+                          const ProfileTileUi(
+                            icon: gearIcon,
+                            title: 'Settings',
+                          ),
+                          const ProfileTileUi(
+                            icon: helpIcon,
+                            title: 'Support',
+                          ),
+                          const ProfileTileUi(
+                            icon: aboutIcon,
+                            title: 'About Riilfit',
+                            tag: VersionNumberCardUi(),
+                          ),
+                          ProfileTileUi(
+                            icon: exitIcon,
+                            title: 'Logout',
+                            onTap: controller.logout,
+                          ),
+                          const Gap(48),
                         ],
                       ),
                     ),
-                    const Spacer(),
-                    const Gap(16),
-                    const ProfileTileUi(
-                      icon: userIcon,
-                      title: 'Edit profile info',
-                    ),
-                    const ProfileTileUi(
-                      icon: gearIcon,
-                      title: 'Settings',
-                    ),
-                    const ProfileTileUi(
-                      icon: helpIcon,
-                      title: 'Support',
-                    ),
-                    const ProfileTileUi(
-                      icon: aboutIcon,
-                      title: 'About Riilfit',
-                      tag: VersionNumberCardUi(),
-                    ),
-                    ProfileTileUi(
-                      icon: exitIcon,
-                      title: 'Logout',
-                      onTap: controller.logout,
-                    ),
-                    const Gap(48),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
