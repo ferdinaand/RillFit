325f9653e56edafbd1d0358b39abd7ddb7cd9a81
ferdinaand <ekpo546@gmail.com>
2023-07-22 23:13:48 +0100

    update

--- lib/src/modules/dashboard/gym/gym_locations/presentation/widgets/gym_location_card.dart
+++ lib/src/modules/dashboard/gym/gym_locations/presentation/widgets/gym_location_card.dart
@@ -15,43 +15,45 @@ class LocationCard extends StatelessWidget {
   String location;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20, bottom: 20),
-      child: SizedBox(
-        height: 75.h,
-        width: 360.h,
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          elevation: 2,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 22),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Image.asset(locationIcon2),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 29),
-                      child: TextUi.bodyLarge(
-                        location.toUpperCase(),
-                        fontWeight: FontWeight.w700,
-                        fontSize: 18,
+    return GestureDetector(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20, bottom: 20),
+        child: SizedBox(
+          height: 75.h,
+          width: 360.h,
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 22),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(locationIcon2),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 29),
+                        child: TextUi.bodyLarge(
+                          location.toUpperCase(),
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 22),
-                  child: Image.asset(
-                    arrowRight,
-                    color: Get.find<ThemeService>().isDarkMode
-                        ? grayScale100
-                        : grayScale900,
+                    ],
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(right: 22),
+                    child: Image.asset(
+                      arrowRight,
+                      color: Get.find<ThemeService>().isDarkMode
+                          ? grayScale100
+                          : grayScale900,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
