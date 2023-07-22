3db11f5f10eaea406f444a01a35b5fd786bfcc09
ferdinaand <ekpo546@gmail.com>
2023-07-22 23:13:56 +0100

    update

--- lib/src/modules/dashboard/profile/controller/profile.controller.dart
+++ lib/src/modules/dashboard/profile/controller/profile.controller.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 import 'package:get/get.dart';
+import 'package:riilfit/src/routing/app_pages.dart';
 import 'package:riilfit/src/domain/base/controller/base.controller.dart';
 
 class ProfileController extends BaseController {
@@ -8,15 +9,18 @@ class ProfileController extends BaseController {
   var isLoading = false.obs;
   Future<void> createVendorAccount() async {}
 
+  //logout
   Future<void> logout() async {
     isLoading.value = true;
 
-    await Future.delayed(Duration(seconds: 20));
+    await Future.delayed(Duration(seconds: 1));
 
     // await AuthApi().logout();
 
-    // await storageService.deleteAllItems();
-
+    await storageService.deleteAllItems();
+    await Get.offAllNamed<void>(
+      Routes.onboarding,
+    );
     Get.snackbar('', 'logged out successfully ');
     isLoading.value = false;
   }
