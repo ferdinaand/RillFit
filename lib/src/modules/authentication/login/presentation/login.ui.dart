01e50f08c77005f69e9372365f8c83631d0db436
ferdinaand <ekpo546@gmail.com>
2023-07-22 23:13:43 +0100

    update

--- lib/src/modules/authentication/login/presentation/login.ui.dart
+++ lib/src/modules/authentication/login/presentation/login.ui.dart
@@ -33,114 +33,113 @@ class LoginUi extends GetView<LoginController> {
           },
           child: Scaffold(
             appBar: const AuthAppbarUi(),
-            body: controller.isLoading.value
-                ? Container(
-                    color: Colors.transparent,
-                    child: const Center(
-                      child: CircularProgressIndicator(
-                        color: primary,
-                      ),
-                    ),
-                  )
-                : SafeArea(
-                    child: Padding(
-                      padding: baseViewPadding,
-                      child: CustomScrollView(
-                        slivers: [
-                          SliverFillRemaining(
-                            hasScrollBody: false,
+            body: SafeArea(
+              child: Padding(
+                padding: baseViewPadding,
+                child: CustomScrollView(
+                  slivers: [
+                    SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Column(
+                        children: [
+                          Expanded(
                             child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Expanded(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Gap(12),
-                                      const TextUi.heading3(
-                                        'Sign In',
-                                      ),
-                                      const Gap(24),
-                                      const LoginFormUi(),
-                                      const Gap(24),
-                                      Obx(
-                                        () => PrimaryButtonUi(
-                                          text: 'Sign In',
-                                          loading: controller.viewState.isBusy,
-                                          onPressed:
-                                              controller.isButtonDisabled.value
-                                                  ? null
-                                                  : controller.login,
-                                        ),
-                                      ),
-                                      const Gap(12),
-                                      InkWell(
-                                        onTap:
-                                            controller.navigateToForgotPassword,
-                                        child: Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                            vertical: 4,
-                                          ),
-                                          child: Obx(
-                                            () => TextUi.bodyMed(
-                                              'Forgot password?',
-                                              color: controller
-                                                      .themeService.isDarkMode
-                                                  ? grayScale100
-                                                  : grayScale700,
-                                              fontWeight: mediumText,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      const Gap(24),
-                                    ],
-                                  ),
-                                ),
-                                const TextUi.bodyMed(
-                                  'Alternatively, Sign in with',
+                                const Gap(12),
+                                const TextUi.heading3(
+                                  'Sign In',
                                 ),
                                 const Gap(24),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    AssetIconButton(
-                                      icon: fbIcon,
-                                      size: 32,
-                                      onTap: controller.loginViaFacebook,
-                                    ),
-                                    const Gap(24),
-                                    AssetIconButton(
-                                      icon: googleIcon,
-                                      size: 32,
-                                      onTap: controller.loginViaGoogle,
-                                    ),
-                                  ],
+                                const LoginFormUi(),
+                                const Gap(24),
+                                Obx(
+                                  () {
+                                    return controller.isLoading.value
+                                        ? const Center(
+                                            child: CircularProgressIndicator(
+                                              color: primary,
+                                            ),
+                                          )
+                                        : PrimaryButtonUi(
+                                            text: 'Sign In',
+                                            loading:
+                                                controller.viewState.isBusy,
+                                            onPressed: controller
+                                                    .isButtonDisabled.value
+                                                ? null
+                                                : controller.login,
+                                          );
+                                  },
                                 ),
-                                const Gap(32),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    const TextUi.bodyMed(
-                                      "Don't have an account? ",
+                                const Gap(12),
+                                InkWell(
+                                  onTap: controller.navigateToForgotPassword,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 4,
                                     ),
-                                    GestureDetector(
-                                      onTap: controller.navigateToRegisterPage,
-                                      child: const TextUi.bodyMed(
-                                        'Sign up',
-                                        color: primary,
-                                        fontWeight: semiBoldText,
+                                    child: Obx(
+                                      () => TextUi.bodyMed(
+                                        'Forgot password?',
+                                        color:
+                                            controller.themeService.isDarkMode
+                                                ? grayScale100
+                                                : grayScale700,
+                                        fontWeight: mediumText,
                                       ),
                                     ),
-                                  ],
+                                  ),
                                 ),
                                 const Gap(24),
                               ],
                             ),
                           ),
+                          const TextUi.bodyMed(
+                            'Alternatively, Sign in with',
+                          ),
+                          const Gap(24),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              AssetIconButton(
+                                icon: fbIcon,
+                                size: 32,
+                                onTap: controller.loginViaFacebook,
+                              ),
+                              const Gap(24),
+                              AssetIconButton(
+                                icon: googleIcon,
+                                size: 32,
+                                onTap: controller.loginViaGoogle,
+                              ),
+                            ],
+                          ),
+                          const Gap(32),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const TextUi.bodyMed(
+                                "Don't have an account? ",
+                              ),
+                              GestureDetector(
+                                onTap: controller.navigateToRegisterPage,
+                                child: const TextUi.bodyMed(
+                                  'Sign up',
+                                  color: primary,
+                                  fontWeight: semiBoldText,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Gap(24),
                         ],
                       ),
                     ),
-                  ),
+                  ],
+                ),
+              ),
+            ),
           ),
         );
       },
@@ -148,16 +147,23 @@ class LoginUi extends GetView<LoginController> {
   }
 }
 
-class showDialog extends StatefulWidget {
-  const showDialog({super.key});
+Future showDialogBox(BuildContext context) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return (Center(
+        child:
+            //     CupertinoActivityIndicator(
 
-  @override
-  State<showDialog> createState() => _showDialogState();
-}
+            //   animating: true,
+            //   color: Colors.blue,
+            // )
 
-class _showDialogState extends State<showDialog> {
-  @override
-  Widget build(BuildContext context) {
-    return CircularProgressIndicator();
-  }
+            CircularProgressIndicator(
+          backgroundColor: Color.fromARGB(126, 6, 6, 6),
+          color: Color.fromARGB(255, 33, 117, 243),
+        ),
+      ));
+    },
+  );
 }
