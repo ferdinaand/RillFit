9d4f7e4c2172be306f77f452787b08da05e1ef17
ferdinaand <ekpo546@gmail.com>
2023-07-24 23:14:03 +0100

    update

--- lib/src/modules/authentication/Models/Nearby_available_drivers.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: file_names
-
-class NearbyAvailableDrivers {
-  String? key;
-  dynamic latitude;
-  dynamic longitude;
-
-  NearbyAvailableDrivers({this.key, this.latitude, this.longitude});
-}
