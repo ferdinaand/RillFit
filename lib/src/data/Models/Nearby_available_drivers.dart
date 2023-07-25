30d29ae34de4cba9c5370b91094cdae7b1385b7d
ferdinaand <ekpo546@gmail.com>
2023-07-25 18:15:26 +0100

    update

---
+++ lib/src/data/Models/Nearby_available_drivers.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: file_names
+
+class NearbyAvailableDrivers {
+  String? key;
+  dynamic latitude;
+  dynamic longitude;
+
+  NearbyAvailableDrivers({this.key, this.latitude, this.longitude});
+}
---
+++ lib/src/data/Models/directDetails.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, camel_case_types
+
+class directDetails {
+  var distance;
+  var duration;
+  var encodedpoints;
+
+  directDetails({this.distance, this.duration, this.encodedpoints});
+}
