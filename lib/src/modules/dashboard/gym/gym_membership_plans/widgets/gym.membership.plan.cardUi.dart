252cc9deab489eea69d70904811855024f75691d
ferdinaand <ekpo546@gmail.com>
2023-07-26 23:17:24 +0100

    update

--- lib/src/modules/dashboard/gym/gym_membership_plans/widgets/gym.membership.plan.cardUi.dart
+++ lib/src/modules/dashboard/gym/gym_membership_plans/widgets/gym.membership.plan.cardUi.dart
@@ -18,6 +18,8 @@ class gymPlanCard extends StatelessWidget {
     required this.logo,
     required this.price,
     required this.planType,
+    required this.OnTap,
+    required this.Id,
     super.key,
   });
   final String description;
@@ -27,107 +29,112 @@ class gymPlanCard extends StatelessWidget {
   final String logo;
   final String price;
   final String planType;
+  final VoidCallback OnTap;
+  final String Id;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 23),
-      child: Container(
-        height: 240,
-        width: 352,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(width: 2, color: primary),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 8, right: 8),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      TextUi.heading1(
-                        price,
-                        fontSize: 32,
-                      ),
-                      Gap(7),
-                      Row(
-                        children: [
-                          TextUi.bodyMed(
-                            'Per month',
-                            fontSize: 14,
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(
-                    height: 19,
-                    width: 73,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(20),
-                        color: Color(0xFFCD7F32)),
-                    child: Center(
-                      child: TextUi.bodyLarge(
-                        planType,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w700,
-                      ),
+    return GestureDetector(
+      onTap: OnTap,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 23),
+        child: Container(
+          height: 240,
+          width: 352,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(width: 2, color: primary),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        TextUi.heading1(
+                          price,
+                          fontSize: 32,
+                        ),
+                        Gap(7),
+                        Row(
+                          children: [
+                            TextUi.bodyMed(
+                              'Per month',
+                              fontSize: 14,
+                            )
+                          ],
+                        ),
+                      ],
                     ),
-                  )
-                ],
-              ),
-              const Gap(23),
-              TextUi.bodyLarge(
-                description,
-                fontSize: 12,
-              ),
-              const Gap(23),
-              Row(
-                children: [
-                  Image.asset(
-                    timeIcon,
-                    color: primary,
-                  ),
-                  const Gap(5),
-                  TextUi.bodyMed(time),
-                ],
-              ),
-              const Gap(14),
-              Row(
-                children: [
-                  Image.asset(
-                    weightIcon,
-                    color: primary,
-                  ),
-                  const Gap(5),
-                  TextUi.bodyMed(features)
-                ],
-              ),
-              const Gap(14),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.asset(
-                        calendarIcon,
-                        color: primary,
+                    Container(
+                      height: 19,
+                      width: 73,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color(0xFFCD7F32)),
+                      child: Center(
+                        child: TextUi.bodyLarge(
+                          planType,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                      const Gap(5),
-                      TextUi.bodyMed(subFeatures),
-                    ],
-                  ),
-                  Image.asset(
-                    riilfitLogoPng,
-                    height: 28.h,
-                  ),
-                ],
-              ),
-              Gap(5)
-            ],
+                    )
+                  ],
+                ),
+                const Gap(23),
+                TextUi.bodyLarge(
+                  description,
+                  fontSize: 12,
+                ),
+                const Gap(23),
+                Row(
+                  children: [
+                    Image.asset(
+                      timeIcon,
+                      color: primary,
+                    ),
+                    const Gap(5),
+                    TextUi.bodyMed(time),
+                  ],
+                ),
+                const Gap(14),
+                Row(
+                  children: [
+                    Image.asset(
+                      weightIcon,
+                      color: primary,
+                    ),
+                    const Gap(5),
+                    TextUi.bodyMed(features)
+                  ],
+                ),
+                const Gap(14),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          calendarIcon,
+                          color: primary,
+                        ),
+                        const Gap(5),
+                        TextUi.bodyMed(subFeatures),
+                      ],
+                    ),
+                    Image.asset(
+                      riilfitLogoPng,
+                      height: 28.h,
+                    ),
+                  ],
+                ),
+                Gap(5)
+              ],
+            ),
           ),
         ),
       ),
