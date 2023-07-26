7982318e44f1b459f4a9c8689b261b4f50ed8efc
ferdinaand <ekpo546@gmail.com>
2023-07-26 22:25:33 +0100

    update

---
+++ lib/src/modules/authentication/login/presentation/widgets/gymOnwerLoginForm.ui.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:riilfit/src/modules/authentication/login/controller/login.controller.dart';
+import 'package:riilfit/src/presentation/widgets.dart';
+
+import '../../controller/gymOwnerLogin.controller.dart';
+
+class GymOwnerLoginFormUi extends GetView<GymOwnerLoginController> {
+  const GymOwnerLoginFormUi({super.key});
+
+  @override
+  GymOwnerLoginController get controller => Get.put(GymOwnerLoginController());
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Form(
+        key: controller.loginFormKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFieldUi(
+              hintText: 'username',
+              controller: controller.usernameController,
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (_) {
+                controller.enableButton();
+              },
+            ),
+            const Gap(12),
+            PasswordFieldUi(
+              hintText: 'Password',
+              controller: controller.passwordController,
+              onChanged: (_) {
+                controller.enableButton();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
