f11618a08e7cdd7c6c9e068c919e30084ea61673
ferdinaand <ekpo546@gmail.com>
2023-07-26 23:16:36 +0100

    update

--- lib/src/modules/authentication/register/presentation/widgets/Gym_Owner_Registration_form_ui.dart
+++ lib/src/modules/authentication/register/presentation/widgets/Gym_Owner_Registration_form_ui.dart
@@ -5,6 +5,7 @@ import 'package:riilfit/src/modules/authentication/login/controller/login.contro
 import 'package:riilfit/src/modules/authentication/register/presentation/widgets/Add_Image.dart';
 import 'package:riilfit/src/presentation/widgets.dart';
 
+import '../../../../../presentation/resources/colors.res.dart';
 import '../../controller/Gym_Owner_Register_controller.dart';
 
 class GymOwnerRegistrationFormUi extends GetView<GymOwnerRegisterController> {
@@ -33,65 +34,100 @@ class GymOwnerRegistrationFormUi extends GetView<GymOwnerRegisterController> {
               hintText: 'FullName',
               controller: controller.fullnameController,
               keyboardType: TextInputType.emailAddress,
-              onChanged: (_) {
-                controller.enableButton();
-              },
+              // onChanged: (_) {
+              //   controller.enableButton();
+              // },
             ),
             Gap(gap),
             TextFieldUi(
               hintText: 'Gym name',
               controller: controller.gymNameController,
               keyboardType: TextInputType.emailAddress,
-              onChanged: (_) {
-                controller.enableButton();
-              },
+              // onChanged: (_) {
+              //   controller.enableButton();
+              // },
             ),
             Gap(gap),
-            TextFieldUi(
-              hintText: 'Gym Location',
-              controller: controller.gymLocatioController,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (_) {
-                controller.enableButton();
-              },
+            TextUi.heading4(
+              'gym location details',
+              fontWeight: FontWeight.w300,
+            ),
+            Gap(5),
+            Row(
+              children: [
+                SizedBox(
+                  width: 110,
+                  child: TextFieldUi(
+                    hintText: 'state',
+                    controller: controller.gymStateController,
+                    keyboardType: TextInputType.emailAddress,
+                    // onChanged: (_) {
+                    //   controller.enableButton();
+                    // },
+                  ),
+                ),
+                const Gap(10),
+                SizedBox(
+                  width: 100,
+                  child: TextFieldUi(
+                    hintText: 'city',
+                    controller: controller.gymCityController,
+                    keyboardType: TextInputType.emailAddress,
+                    // onChanged: (_) {
+                    //   controller.enableButton();
+                    // },
+                  ),
+                ),
+                const Gap(10),
+                SizedBox(
+                  width: 120,
+                  child: TextFieldUi(
+                    hintText: 'Address',
+                    controller: controller.gymAddressController,
+                    keyboardType: TextInputType.emailAddress,
+                    // onChanged: (_) {
+                    //   controller.enableButton();
+                    // },
+                  ),
+                ),
+              ],
             ),
             Gap(gap),
             TextFieldUi(
               hintText: 'Phone number',
               controller: controller.phoneController,
               keyboardType: TextInputType.emailAddress,
-              onChanged: (_) {
-                controller.enableButton();
-              },
-            ),
-            Gap(gap),
-            TextFieldUi(
-              hintText: 'Email',
-              controller: controller.emailOrPhoneController,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (_) {
-                controller.enableButton();
-              },
+              // onChanged: (_) {
+              //   controller.enableButton();
+              // },
             ),
             Gap(gap),
             PasswordFieldUi(
               hintText: 'password',
               controller: controller.passwordController,
-              onChanged: (_) {
-                controller.enableButton();
-              },
+              // onChanged: (_) {
+              //   controller.enableButton();
+              // },
             ),
             Gap(30),
             Center(child: const AddImage()),
             const Gap(40),
             Obx(
-              () => PrimaryButtonUi(
-                text: 'Sign up',
-                loading: controller.viewState.isBusy,
-                onPressed: controller.isButtonDisabled.value
-                    ? null
-                    : controller.signup,
-              ),
+              () => controller.isLoading.value
+                  ? const Center(
+                      child: CircularProgressIndicator(
+                        color: primary,
+                      ),
+                    )
+                  : PrimaryButtonUi(
+                      text: 'Sign up',
+                      loading: controller.viewState.isBusy,
+                      onPressed:
+                          //  controller.isButtonDisabled.value
+                          //     ? null
+                          //     :
+                          controller.signup,
+                    ),
             ),
           ],
         ),
