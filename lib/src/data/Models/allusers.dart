726322515d5edaca7fd011392b33359e47d8660a
ferdinaand <ekpo546@gmail.com>
2023-07-27 23:37:19 +0100

    update

--- lib/src/data/Models/allusers.dart
+++ lib/src/data/Models/allusers.dart
@@ -3,32 +3,40 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-class Users {
-  String? id;
-  String? email;
-  String? name;
-  dynamic phone;
+// class Users {
+//   String? id;
+//   String? email;
+//   String? name;
+//   dynamic phone;
 
-  Users({this.id, this.email, this.name, this.phone});
+//   Users({this.id, this.email, this.name, this.phone});
 
-  Users.fromSnapshot(DataSnapshot dataSnapshot) {
-    Map<dynamic, dynamic> map = dataSnapshot.value! as Map<dynamic, dynamic>;
-    id = dataSnapshot.key;
-    email = map["email"] as String;
-    name = map["firstname"] as String;
-    phone = map["Phone No"] as String;
-  }
-}
+//   Users.fromSnapshot(DataSnapshot dataSnapshot) {
+//     Map<dynamic, dynamic> map = dataSnapshot.value! as Map<dynamic, dynamic>;
+//     id = dataSnapshot.key;
+//     email = map["email"] as String;
+//     name = map["firstname"] as String;
+//     phone = map["Phone No"] as String;
+//   }
+// }
 
-class userSearch {
-  String? id;
-  String? email;
-  String? name;
-  userSearch({this.id, this.email, this.name});
+// class userSearch {
+//   String? id;
+//   String? email;
+//   String? name;
+//   userSearch({this.id, this.email, this.name});
 
-  // userSearch.fromSnapshot(DataSnapshot dataSnapshot) {
-  //   Map<dynamic, dynamic> map = dataSnapshot.value! as Map<dynamic, dynamic>;
+// userSearch.fromSnapshot(DataSnapshot dataSnapshot) {
+//   Map<dynamic, dynamic> map = dataSnapshot.value! as Map<dynamic, dynamic>;
 
-  //   id = map['user_id'];
-  // }
+//   id = map['user_id'];
+// }
+// }
+
+class locations {
+  String? city;
+  String? state;
+  String? address;
+
+  locations({this.city, this.state, this.address});
 }
