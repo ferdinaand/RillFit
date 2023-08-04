c1d3a92c3bf296d89b9b3b03edf7275677a9d0fa
ferdinaand <ekpo546@gmail.com>
2023-08-04 23:44:36 +0100

    update

--- lib/src/data/Models/directDetails.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, camel_case_types
-
-class directDetails {
-  var distance;
-  var duration;
-  var encodedpoints;
-
-  directDetails({this.distance, this.duration, this.encodedpoints});
-}
