6c0344160f460c267da71f39c3b0bb20b3d6e30a
ferdinaand <ekpo546@gmail.com>
2023-08-04 23:44:52 +0100

    update

---
+++ lib/src/data/Models/GymDetails.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, camel_case_types
+
+class gymDetails {
+  gymDetails({
+    this.id,
+    this.name,
+    this.status,
+    this.rating,
+    this.description,
+    this.openingTime,
+    this.closingTime,
+    this.phoneNumber,
+    this.website,
+  });
+
+  String? id;
+  String? name;
+  String? status;
+  String? rating;
+  String? description;
+  String? openingTime;
+  String? closingTime;
+  String? phoneNumber;
+  String? website;
+  String? instagram;
+  String? facebook;
+  String? snapchat;
+  String? whatsapp;
+}
