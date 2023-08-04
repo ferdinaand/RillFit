a06bfaedc4ef633c3d045c966c2507764d9ff8e4
ferdinaand <ekpo546@gmail.com>
2023-08-04 23:45:44 +0100

    update

---
+++ lib/src/data/Models/Users.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: file_names
+class usersDetails {
+  usersDetails({
+    this.fullName,
+    this.phoneNumber,
+    this.profileImage,
+    this.userName,
+    this.userId,
+  });
+
+  String? fullName;
+  String? phoneNumber;
+  String? profileImage;
+  String? userName;
+  String? userId;
+}
