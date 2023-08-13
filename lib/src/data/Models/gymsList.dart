bd6afc382b23adf8cc342da6c1933446d6e27e7e
ferdinaand <ekpo546@gmail.com>
2023-08-13 22:01:30 +0100

    update

---
+++ lib/src/data/Models/gymsList.dart
@@ -0,0 +1,15 @@
+class GymList {
+  GymList({
+    this.id,
+    this.name,
+    this.logo,
+    this.city,
+    this.address,
+  });
+
+  String? id;
+  String? name;
+  String? logo;
+  String? address;
+  String? city;
+}
