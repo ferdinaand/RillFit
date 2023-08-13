6711538e5b0c0c8437ac69a9a7c5f131701f6962
ferdinaand <ekpo546@gmail.com>
2023-08-13 22:01:03 +0100

    update

--- lib/src/modules/dashboard/gym/gym_locations/controller/gym_locations_controller.dart
+++ lib/src/modules/dashboard/gym/gym_locations/controller/gym_locations_controller.dart
@@ -1,9 +1,20 @@
-import 'package:get/get.dart';
-import 'package:riilfit/src/domain/base/controller/base.controller.dart';
+import 'dart:convert';
 
+import 'package:get/get.dart';
+import 'package:riilfit/src/data/Models/GymLocations.dart';
+import 'package:riilfit/src/domain/base/controller/base.controller.dart';
+import 'package:http/http.dart' as http;
+import 'package:riilfit/src/utils/config.dart';
 import '../../../../../routing/app_pages.dart';
 
 class GymLocationsController extends BaseController {
+  void onInit() {
+    getGymLocations();
+
+    super.onInit();
+  }
+
+  var isLoading = false.obs;
   final _currentIndex = 0.obs;
   int get currentIndex => _currentIndex.value;
   set currentIndex(int index) {
@@ -12,7 +23,28 @@ class GymLocationsController extends BaseController {
 
   void gymsInLocation() {
     navigationService.navigateTo(
-      Routes.gym,
+      isLoggedIn ? Routes.gym : Routes.login,
     );
+  }
+
+  void getGymLocations() async {
+    // isLoading.value = true;
+
+    // await Future.delayed(Duration(seconds: 3));
+
+    var response = await http.get(
+      Uri.parse('https://riilfit-api.vercel.app//gyms/available-locations'),
+    );
+    // isLoading.value = false;
+
+    final jsonResponse = jsonDecode(response.body);
+    var data = jsonResponse['data'];
+    var items = data['items'];
+    print(items[0]);
+    if (jsonResponse['statusCode'] == '200') {
+      GymLocations gymLocations = GymLocations();
+      gymLocations.city = jsonResponse['city'].toString();
+      gymLocations.state = jsonResponse['state'].toString();
+    }
   }
 }
