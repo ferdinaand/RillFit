1b4f5e26327ad5ecbba925c9d9f9624e1d0568d6
ferdinaand <ekpo546@gmail.com>
2023-08-24 23:58:29 +0100

    update

--- lib/src/data/Models/GymLocations.dart
+++ lib/src/data/Models/GymLocations.dart
@@ -1,3 +1,11 @@
+// ignore_for_file: avoid_dynamic_calls
+
+import 'dart:convert';
+
+// List<GymLocations> gymLocationsFromJson(String str) => List<GymLocations>.from(
+//       json.decode(str) as Iterable<dynamic>,
+//     );
+
 class GymLocations {
   GymLocations({
     required this.state,
@@ -7,7 +15,7 @@ class GymLocations {
   String? state;
   String? city;
 
-  factory GymLocations.fromJson(Map<String, dynamic> JsonData) => GymLocations(
+  factory GymLocations.fromJson(dynamic JsonData) => GymLocations(
         state: JsonData['data']['items']['state'] as String?,
         city: JsonData['data']['items']['city'] as String?,
       );
