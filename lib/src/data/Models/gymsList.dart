80858100ab821834fdf56aeb725b7949d94cf105
ferdinaand <ekpo546@gmail.com>
2023-08-26 10:52:48 +0100

    UPDATE

--- lib/src/data/Models/gymsList.dart
+++ lib/src/data/Models/gymsList.dart
@@ -1,15 +1,23 @@
 class GymList {
   GymList({
-    this.id,
-    this.name,
-    this.logo,
-    this.city,
-    this.address,
+    required this.id,
+    required this.name,
+    required this.logo,
+    required this.city,
+    required this.address,
   });
 
-  String? id;
-  String? name;
-  String? logo;
-  String? address;
-  String? city;
+  String id;
+  String name;
+  String logo;
+  String address;
+  String city;
+
+  factory GymList.fromJson(dynamic JsonData) => GymList(
+        id: JsonData['data']['items']['id'] as String,
+        name: JsonData['data']['items']['name'] as String,
+        city: JsonData['data']['items']['locations']['city'] as String,
+        logo: JsonData['data']['items']['thumbnail'] as String,
+        address: JsonData['data']['items']['locations']['address'] as String,
+      );
 }
