0567f4c3ac595e5556f2b1b4880ed8d41bfb6ef7
ferdinaand <ekpo546@gmail.com>
2023-08-26 10:52:56 +0100

    UPDATE

--- lib/src/modules/dashboard/gym/gyms_list/presentation/gym.ui.dart
+++ lib/src/modules/dashboard/gym/gyms_list/presentation/gym.ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:riilfit/src/data/dummy/member_services.dummy.dart';
 import 'package:riilfit/src/modules/dashboard/gym/gyms_list/controller/gym.controller.dart';
@@ -7,16 +8,22 @@ import 'package:riilfit/src/modules/dashboard/gym/gyms_list/presentation/widgets
 import 'package:riilfit/src/presentation/resources/res.dart';
 import 'package:riilfit/src/presentation/widgets.dart';
 
-class GymUi extends GetView<GymController> {
+import '../../gym_locations/controller/gym_locations_controller.dart';
+
+class GymUi extends GetView<GymLocationsController> {
   const GymUi({
     super.key,
   });
 
   @override
-  GymController get controller => Get.put(GymController());
+  GymLocationsController get controller => Get.put(GymLocationsController());
 
   @override
   Widget build(BuildContext context) {
+    // Clear the list before displaying data for a new location
+
+    print(controller.thisGymList.length);
+
     return GestureDetector(
       onTap: () {
         final currentFocus = FocusScope.of(context);
@@ -28,38 +35,77 @@ class GymUi extends GetView<GymController> {
         appBar: const MainAppbarUi(
           title: 'Gyms',
         ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const Gap(16),
-                Padding(
-                  padding: baseViewPadding,
-                  child: const GymSearchFieldUi(),
-                ),
-                const Gap(16),
-                SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10, right: 10),
-                    child: Column(
-                      children: [
-                        ...gServices.asMap().entries.map(
-                          (entry) {
-                            final service = entry.value;
-                            return GymCardUi(
-                              service: service,
-                            );
-                          },
-                        ),
-                      ],
+        body: CustomScrollView(
+          slivers: [
+            // SliverAppBar with search bar
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              floating: false, // Set to true if you want it to appear on scroll
+              pinned: true, // Set to true if you want it to remain at the top
+              expandedHeight: 100, // Adjust as needed
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: SizedBox(
+                  height: 40.h,
+                  width: 230.w,
+                  child: TextField(
+                    // Your search bar widget here
+                    decoration: InputDecoration(
+                      hintText: 'Search Gyms',
                     ),
                   ),
-                )
-              ],
+                ),
+              ),
             ),
-          ),
+
+            // SliverList for the gym list
+            Obx(
+              () => controller.isGymListLoading.value
+                  ? SliverToBoxAdapter(
+                      child: Center(
+                          child: CircularProgressIndicator(
+                        color: primary,
+                      )),
+                    )
+                  : SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (_, index) {
+                          final gym = controller.thisGymList[index];
+
+                          return Padding(
+                            padding: baseViewPadding,
+                            child: GymCardUi(
+                              city: gym.city,
+                              address: gym.address,
+                              logo: gym.logo,
+                              name: gym.name,
+                            ),
+                          );
+                        },
+                        childCount: controller.thisGymList.length,
+                      ),
+                    ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
+//  SingleChildScrollView(
+//                   child: Padding(
+//                     padding: const EdgeInsets.only(left: 10, right: 10),
+//                     child: Column(
+//                       children: [
+//                         ...gServices.asMap().entries.map(
+//                           (entry) {
+//                             final service = entry.value;
+//
+//                           },
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 )
