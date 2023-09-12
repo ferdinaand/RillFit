d626bbdb8427d69ced796498c23960b2540ddfd7
ferdinaand <ekpo546@gmail.com>
2023-09-12 23:40:16 +0100

    update

--- lib/src/modules/dashboard/gym/gym_membership_plans/controllers/gym-membership_plans_controller.dart
+++ lib/src/modules/dashboard/gym/gym_membership_plans/controllers/gym-membership_plans_controller.dart
@@ -7,6 +7,7 @@ import 'package:flutterwave_standard/models/requests/customer.dart';
 import 'package:flutterwave_standard/models/requests/customizations.dart';
 import 'package:flutterwave_standard/models/responses/charge_response.dart';
 import 'package:get/get.dart';
+import 'package:mono_flutter/mono_flutter.dart';
 import 'package:riilfit/src/domain/base/controller/base.controller.dart';
 import 'package:http/http.dart' as http;
 import '../../../../../routing/app_pages.dart';
@@ -15,7 +16,7 @@ import 'dart:convert';
 
 class GymPlansController extends BaseController {
   final iD = ''.obs;
-  var isLoading = false.obs;
+  final isLoading = false.obs;
   final _currentIndex = 0.obs;
   int get currentIndex => _currentIndex.value;
   set currentIndex(int index) {
@@ -46,6 +47,53 @@ class GymPlansController extends BaseController {
     isButtonDisabled.value == planController.text.isEmpty;
     return;
   }
+
+  void handlePayment(BuildContext context) async {
+    MonoFlutter().launch(
+      context,
+      'test_pk_ku8af8e0gc6hibayqx5b',
+
+      // reAuthCode: 'code_7ZSg9UB0sZsMSL3ygb6X',
+      reference: DateTime.now().millisecondsSinceEpoch.toString(),
+      config: {
+        "selectedInstitution": {
+          "id": "5f2d08bf60b92e2888287703",
+          "auth_method": "internet_banking"
+        }
+      },
+      onEvent: (event, data) {
+        if (kDebugMode) print('event: $event, data: $data');
+      },
+      onClosed: () {
+        if (kDebugMode) print('Modal closed');
+      },
+      onLoad: () {
+        if (kDebugMode) print('Mono loaded successfully');
+      },
+      onSuccess: (code) {
+        if (kDebugMode) print('Mono Success $code');
+        getGymQr();
+      },
+    );
+  }
+
+  //select a city when clicked
+  final RxString selectedPlanId = ''.obs;
+
+  final RxString selectedPlanPrice = ''.obs;
+
+  final RxString selectedPlanType = ''.obs;
+
+  final RxString subscribersName = ''.obs;
+
+  void setSelectedPlan(
+      String plan, String price, String type, String subscriberName) {
+    selectedPlanId.value = plan;
+    selectedPlanPrice.value = price;
+    selectedPlanType.value = type;
+    subscribersName.value = subscriberName;
+  }
+//++
 
   Future<void> getGymQr() async {
     isLoading.value = true;
