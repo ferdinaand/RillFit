2d12a5349da206aa285632b5994ebe8cda87f614
ferdinaand <ekpo546@gmail.com>
2023-09-12 23:39:57 +0100

    update

--- lib/src/data/Models/GymDetails.dart
+++ lib/src/data/Models/GymDetails.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, camel_case_types
+// ignore_for_file: prefer_typing_uninitialized_variables, camel_case_types, avoid_dynamic_calls, sort_constructors_first
 
 class gymDetails {
   gymDetails({
@@ -11,6 +11,15 @@ class gymDetails {
     this.closingTime,
     this.phoneNumber,
     this.website,
+    this.facebook,
+    this.instagram,
+    this.snapchat,
+    this.whatsapp,
+    this.address,
+    this.country,
+    this.city,
+    this.state,
+    this.thumbnail,
   });
 
   String? id;
@@ -26,8 +35,28 @@ class gymDetails {
   String? facebook;
   String? snapchat;
   String? whatsapp;
+  String? address;
+  String? country;
+  String? state;
+  String? city;
+  String? thumbnail;
 
   factory gymDetails.fromJson(dynamic JsonData) => gymDetails(
-        id: JsonData['data']['items']['state'] as String?,
+        // openingTime:
+        //     JsonData['data']['accessPeriod']['time']['start'] as String?,
+        // closingTime: JsonData['data']['accessPeriod']['time']['end'] as String?,
+        // id: JsonData['data']['_id'] as String?,
+        // website: JsonData['data']['website'] as String?,
+        // description: JsonData['data']['about'] as String?,
+        // facebook: JsonData['data']['socialLinks']['facebook'] as String?,
+        // snapchat: JsonData['data']['socialLinks']['snapchat'] as String?,
+        // instagram: JsonData['data']['socialLinks']['instagram'] as String?,
+        name: JsonData['name'] as String?,
+        country: JsonData['locations'][0]['country'] as String,
+        address: JsonData['locations'][0]['address'] as String?,
+        // state: JsonData['data']['locations']['state'] as String?,
+        // city: JsonData['data']['locations']['city'] as String?,
+        // phoneNumber: JsonData['data']['phone'] as String?,
+        // thumbnail: JsonData['data']['thumbnail'] as String?,
       );
 }
