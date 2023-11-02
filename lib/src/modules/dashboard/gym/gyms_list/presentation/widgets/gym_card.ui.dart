01a76b7f2c1f78547283d8fd3c4450b4075ed4b3
ferdinaand <ekpo546@gmail.com>
2023-11-02 13:55:06 +0100

    RillFit/ Update

--- lib/src/modules/dashboard/gym/gyms_list/presentation/widgets/gym_card.ui.dart
+++ lib/src/modules/dashboard/gym/gyms_list/presentation/widgets/gym_card.ui.dart
@@ -37,126 +37,157 @@ class GymCardUi extends GetView<GymController> {
     return GestureDetector(
       onTap: ontap,
       child: Padding(
-        padding: const EdgeInsets.only(bottom: 20),
-        child: SizedBox(
-          height: 160.h,
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  image: DecorationImage(
-                    image: NetworkImage(logo),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(20),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Container(
-                        height: 24.h,
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 4,
-                          vertical: 0,
-                        ).w,
-                        decoration: BoxDecoration(
-                          color: white.withOpacity(1),
-                          borderRadius: const BorderRadius.all(
-                            xsmallRadius,
-                          ),
-                        ),
-                        child: Icon(
-                          isFavorite
-                              ? Icons.favorite
-                              : Icons.favorite_border_rounded,
-                          color: isFavorite ? wineColor : grayScale100,
-                          size: 24,
-                        ),
+        padding: const EdgeInsets.only(
+          bottom: 20,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 270.h,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      image: DecorationImage(
+                        image: NetworkImage(logo),
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    const Spacer(),
-                    Row(
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        TextUi.heading4(
-                          name,
-                          color: grayScale500,
-                        ),
-                        SizedBox(
-                          width: 11,
-                        ),
-                        Container(
-                          height: 20.h,
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 10,
-                            vertical: 0,
-                          ).w,
-                          decoration: BoxDecoration(
-                            color: grayScale700.withOpacity(.9),
-                            borderRadius: const BorderRadius.all(
-                              xsmallRadius,
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                            height: 24.h,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 4,
+                              vertical: 0,
+                            ).w,
+                            decoration: BoxDecoration(
+                              color: white.withOpacity(1),
+                              borderRadius: const BorderRadius.all(
+                                xsmallRadius,
+                              ),
+                            ),
+                            child: Icon(
+                              // isFavorite
+                              //     ?
+                              Icons.favorite
+                              // : Icons.favorite_border_rounded
+                              ,
+                              color:
+                                  // isFavorite ?
+                                  wineColor
+                              //  : grayScale100
+                              ,
+                              size: 24,
                             ),
                           ),
-                          child: TextUi.bodyMed(
-                            isClosed ? 'CLOSED' : 'OPEN',
-                            color: isClosed
-                                ? grayScale900
-                                : successDark, //use grayScale900 when closed
-                            fontWeight: semiBoldText,
-                            height: (16 / 14).w,
-                          ),
-                        ),
-                      ],
-                    ),
-                    TextUi.bodySmall(
-                      '$address,$city ',
-                      fontSize: 14,
-                      color: grayScale500,
-                    ),
-                    const Gap(4),
-                    const Gap(4),
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.star,
-                          color: starColor,
-                        ),
-                        const Icon(
-                          Icons.star,
-                          color: starColor,
-                        ),
-                        const Icon(
-                          Icons.star,
-                          color: starColor,
-                        ),
-                        const Icon(
-                          Icons.star_half,
-                          color: starColor,
-                        ),
-                        const Icon(
-                          Icons.star,
-                          color: white,
                         ),
                         const Spacer(),
+                        const Gap(4),
+                        const Gap(4),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.star,
+                              color: starColor,
+                            ),
+                            const Icon(
+                              Icons.star,
+                              color: starColor,
+                            ),
+                            const Icon(
+                              Icons.star,
+                              color: starColor,
+                            ),
+                            const Icon(
+                              Icons.star_half,
+                              color: starColor,
+                            ),
+                            const Icon(
+                              Icons.star,
+                              color: white,
+                            ),
+                            const Spacer(),
+                          ],
+                        ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      TextUi.heading4(
+                        name.toUpperCase(),
+                        color: grayScale900,
+                      ),
+                      SizedBox(
+                        width: 11,
+                      ),
+                      // Container(
+                      //   height: 20.h,
+                      //   padding: const EdgeInsets.symmetric(
+                      //     horizontal: 10,
+                      //     vertical: 0,
+                      //   ).w,
+                      //   decoration: BoxDecoration(
+                      //     color: grayScale700.withOpacity(.9),
+                      //     borderRadius: const BorderRadius.all(
+                      //       xsmallRadius,
+                      //     ),
+                      //   ),
+                      //   child:
+                      TextUi.bodyMed(
+                        isClosed ? 'CLOSED' : 'OPEN',
+                        color: isClosed
+                            ? grayScale900
+                            : successDark, //use grayScale900 when closed
+                        fontWeight: semiBoldText,
+                        height: (16 / 14).w,
+                      ),
+                      // ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white54,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: TextUi.bodyMed(
+                          '$address, $city',
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: grayScale900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
