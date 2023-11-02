36adb3d1d95671da13d8c4590529be37bfead623
ferdinaand <ekpo546@gmail.com>
2023-11-02 13:54:10 +0100

    RillFit/ Update

--- lib/src/modules/authentication/register/controller/register.controller.dart
+++ lib/src/modules/authentication/register/controller/register.controller.dart
@@ -32,6 +32,7 @@ class RegisterController extends BaseController {
   late GlobalKey<FormState> registerFormKey;
   var isLoading = false.obs;
   SharedPreferences? pref;
+  var gym_name = ''.obs;
 
   @override
   void onInit() {
@@ -43,8 +44,23 @@ class RegisterController extends BaseController {
     super.onInit();
   }
 
+  // Future<void> selectGym() async {
+  //   final box = await Hive.openBox('userData');
+
+  // }
+
   void createBox() async {
-    box1 = await Hive.openBox('userData');
+    final box = await Hive.openBox('userData');
+    if (box.containsKey('gym_name')) {
+      final gymName = box.get('gym_name', defaultValue: '');
+      final gymId = box.get('gym_id', defaultValue: '');
+      print(gymId);
+      print('Fetched gym_name: $gymName');
+      gym_name.value = gymName.toString();
+      gymNameController.text = gymName.toString();
+    } else {
+      print('Key "gym_name" does not exist in userData box.');
+    }
   }
 
   void initSharedPref() async {
@@ -91,21 +107,26 @@ class RegisterController extends BaseController {
   }
 
 /////persiting user login in data with hive
-  void persistData() {
-    box1
+  Future<void> persistData() async {
+    final box = await Hive.openBox('userData');
+    box
       ..put('phone', phoneController.text)
       ..put('fullname', nameController.text);
   }
 
 //signup with node js backend
   Future<void> signUp() async {
+    final box = await Hive.openBox('userData');
+    final gymId = box.get('gym_id', defaultValue: '');
+    // gym_name.value = gymName.toString();
     var signupBody = {
       'fullName': nameController.text,
       'username': usernameController.text,
       'phone': phoneController.text,
       'password': passwordController.text,
-      'gym': gymNameController.text
+      'gym': gymId
     };
+    print(gymId);
     firstName = nameController.text;
     userName = usernameController.text;
     phoneNumber = phoneController.text;
@@ -143,129 +164,6 @@ class RegisterController extends BaseController {
     }
     isLoading.value = false;
   }
-
-  //signup with firebase
-  // Future<void> signUp() async {
-  //   try {
-  //     await FirebaseAuth.instance
-  //         .createUserWithEmailAndPassword(
-  //           email: emailController.text.trim(),
-  //           password: passwordController.text.trim(),
-  //         )
-
-  //         //add user details to firestore
-  //         .then(
-  //           (value) => {
-  //             postDetailsToFirestore(
-  //               phoneController.text,
-  //               emailController.text,
-  //               nameController.text,
-  //               passwordController.text,
-  //             )
-  //           },
-  //         );
-  //   } on FirebaseAuthException catch (e) {
-  //     Get.snackbar('Error: ', ' ${e.toString()}');
-  //   }
-  // }
-
-  // // ignore: always_declare_return_types, inference_failure_on_function_return_type
-  // postDetailsToFirestore(
-  //   String phoneNo,
-  //   String email,
-  //   String firstname,
-  //   String lastname,
-  // ) async {
-  //   final user = FirebaseAuth.instance.currentUser;
-  //   final userRef = FirebaseDatabase.instance.ref().child('user');
-  //   final phoneNoReceiver = phoneController.text;
-  //   if (user != null) {
-  //     await pref!.setString('user id', user.toString());
-  //   }
-  //   await userRef.child(user!.uid).set({
-  //     'id': user.uid,
-  //     'Phone No': '+234$phoneNoReceiver',
-  //     'email': emailController.text,
-  //     'firstname': firstname,
-  //     'lastname': lastname
-  //   });
-
-  //   navigateToLoginPage();
-  // }
-
-  // DriverPhoneNo = PhoneNoController;
-  // currentFirebaseUser = driveruser;
-
-  //   // try {
-  //   //   //Validate form
-  //   //   registerFormKey.currentState!.save();
-  //   //   if (!registerFormKey.currentState!.validate()) {
-  //   //     showFlushBar(
-  //   //       message: 'Kindly fix validation issues',
-  //   //     );
-  //   //     return;
-  //   //   }
-  //   //   viewState = ViewState.busy;
-
-  //   //   final registerDto = RegisterDto(
-  //   //     email: emailController.text,
-  //   //     phoneNumber: phoneController.text.addCountryCode,
-  //   //     fullName: nameController.text,
-  //   //     password: passwordController.text,
-  //   //   );
-
-  //   //   final res = await AuthApi().createAccount(
-  //   //     registerDto: registerDto,
-  //   //   );
-
-  //   //   if (res.success) {
-  //   //     //store token
-  //   //     await storageService.cacheAuthToken(
-  //   //       res.payload['token'] as String,
-  //   //     );
-
-  //   //     //store user
-  //   //     await storageService.cacheCustomer(
-  //   //       jsonEncode(res.payload['user']),
-  //   //     );
-  //   //     showFlushBar(
-  //   //       message: res.message ??
-  //   //           'Signed up successfully, We have sent a verification code to ${phoneController.text}',
-  //   //       errorType: ErrorType.success,
-  //   //     );
-
-  //   //     unawaited(
-  //   //       Get.toNamed<void>(
-  //   //         Routes.verifyPhone,
-  //   //       ),
-  //   //     );
-
-  //   //     viewState = ViewState.idle;
-  //   //   } else {
-  //   //     if (res.message == passwordTooWeak) {
-  //   //       showFlushBar(
-  //   //         message: passwordTooWeak,
-  //   //       );
-  //   //     }
-  //   //     showFlushBar(
-  //   //       message: res.message ?? '',
-  //   //     );
-  //   //     viewState = ViewState.idle;
-  //   //   }
-  //   //   return;
-  //   // } catch (e, s) {
-  //   //   log(
-  //   //     e.toString(),
-  //   //     stackTrace: s,
-  //   //   );
-  //   //   showFlushBar(
-  //   //     message: errorMessage,
-  //   //   );
-  //   //   viewState = ViewState.idle;
-  //   // } finally {
-  //   //   viewState = ViewState.idle;
-  //   // }
-  // }
 
   Future<void> loginViaFacebook() async {}
   Future<void> loginViaGoogle() async {}
