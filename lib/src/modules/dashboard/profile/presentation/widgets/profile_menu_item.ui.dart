f19b82eea99bfb9ecac5d356b9fdfa138ac26fa6
ferdinaand <ekpo546@gmail.com>
2023-11-02 13:56:13 +0100

    RillFit/ Update

--- lib/src/modules/dashboard/profile/presentation/widgets/profile_menu_item.ui.dart
+++ lib/src/modules/dashboard/profile/presentation/widgets/profile_menu_item.ui.dart
@@ -12,59 +12,70 @@ class ProfileTileUi extends StatelessWidget {
     required this.title,
     this.onTap,
     this.tag,
+    this.menuIconColor,
   });
 
   final String icon;
   final String title;
   final VoidCallback? onTap;
   final Widget? tag;
+  final Color? menuIconColor;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap ?? () {},
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          vertical: 8,
-          horizontal: 20,
-        ),
-        margin: const EdgeInsets.symmetric(
-          vertical: 10,
-        ),
-        child: Row(
-          children: [
-            Obx(
-              () => SizedBox.square(
-                dimension: 24,
-                child: SvgPicture.asset(
-                  icon,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTap ?? () {},
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 20,
+          ),
+          margin: const EdgeInsets.symmetric(
+            vertical: 10,
+          ),
+          child: Row(
+            children: [
+              // Obx(
+              // () =>
+              Container(
+                height: 35,
+                width: 35,
+                decoration: BoxDecoration(
+                    color: menuIconColor,
+                    borderRadius: BorderRadius.circular(6)),
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: SvgPicture.asset(
+                    icon,
+                    color: white,
+                    width: 14,
+                    height: 14,
+                  ),
+                ),
+              ),
+              // ),
+              const SizedBox(width: 20),
+              TextUi.bodyMed(
+                title,
+                height: 1.1,
+                fontWeight: mediumText,
+              ),
+              const Spacer(),
+              if (tag != null) ...[
+                tag!,
+              ],
+              Obx(
+                () => Icon(
+                  Icons.arrow_forward_ios,
                   color: Get.find<ThemeService>().isDarkMode
                       ? grayScale100
                       : grayScale900,
-                  width: 24,
-                  height: 24,
+                  size: 16,
                 ),
               ),
-            ),
-            const SizedBox(width: 20),
-            TextUi.bodyMed(
-              title,
-              height: 1.1,
-              fontWeight: mediumText,
-            ),
-            const Spacer(),
-            if (tag != null) ...[
-              tag!,
             ],
-            Obx(
-              () => Icon(
-                Icons.arrow_forward_ios,
-                color: Get.find<ThemeService>().isDarkMode
-                    ? grayScale100
-                    : grayScale900,
-                size: 16,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
