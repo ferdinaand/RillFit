ce7bff4fd77a3ccdf21ef136abcf816c1e4324b7
ferdinaand <ekpo546@gmail.com>
2023-11-02 13:54:28 +0100

    RillFit/ Update

--- lib/src/modules/dashboard/gym/gym_membership_plans/controllers/gym-membership_plans_controller.dart
+++ lib/src/modules/dashboard/gym/gym_membership_plans/controllers/gym-membership_plans_controller.dart
@@ -4,8 +4,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl.dart';
 import 'package:riilfit/src/data/Models/gym.plans.models.dart';
 import 'package:riilfit/src/presentation/resources/colors.res.dart';
+import 'package:riilfit/src/utils/config.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 // Import for iOS features.
@@ -18,6 +20,7 @@ import 'package:get/get.dart';
 import 'package:mono_flutter/mono_flutter.dart';
 import 'package:riilfit/src/domain/base/controller/base.controller.dart';
 import 'package:http/http.dart' as http;
+import '../../../../../data/Models/notificationModel.dart';
 import '../../../../../routing/app_pages.dart';
 import 'dart:async';
 import 'dart:convert';
@@ -36,23 +39,23 @@ class GymPlansController extends BaseController {
   var phoneNumber = ''.obs;
   var userName = ''.obs;
 
-  String flutterWaveTest = '''
- <form method="POST" action="https://checkout.flutterwave.com/v3/hosted/pay">
-  <div>
-    Your order is ₦40000
-  </div>
-  <input type="hidden" name="public_key" value="FLWPUBK-58cc63c00e129204fb4e94ef95bb781f-X" />
-  <input type="hidden" name="customer[email]" value="jessepinkman@walterwhite.org" />
-  <input type="hidden" name="customer[name]" value="Jesse Pinkman" />
-  <input type="hidden" name="tx_ref" value="bitethtx-019203" />
-  <input type="hidden" name="amount" value="40000" />
-  <input type="hidden" name="currency" value="NGN" />
-  <input type="hidden" name="meta[token]" value="54" />
-  <input type="hidden" name="redirect_url" value="https://google.com" />
-   <input type="hidden" name="payment_options" value="card" />
-  <button type="submit" id="start-payment-button">Pay Now</button>
-  
-</form> ''';
+//   String flutterWaveTest = '''
+//  <form method="POST" action="https://checkout.flutterwave.com/v3/hosted/pay">
+//   <div>
+//     Your order is N10
+//   </div>
+//   <input type="hidden" name="public_key" value="FLWPUBK-58cc63c00e129204fb4e94ef95bb781f-X" />
+//   <input type="hidden" name="customer[email]" value="jessepinkman@walterwhite.org" />
+//   <input type="hidden" name="customer[name]" value="${fullname.value}" />
+//   <input type="hidden" name="tx_ref" value="bitethtx-019203" />
+//   <input type="hidden" name="amount" value="10" />
+//   <input type="hidden" name="currency" value="NGN" />
+//   <input type="hidden" name="meta[token]" value="54" />
+//   <input type="hidden" name="redirect_url" value="https://riilfit-api.vercel.app/debit-cards/new/?userId= " />
+//    <input type="hidden" name="payment_options" value="card" />
+//   <button type="submit" id="start-payment-button">Pay Now</button>
+
+// </form> ''';
 
   void getPlanId() {}
 
@@ -62,8 +65,21 @@ class GymPlansController extends BaseController {
     );
   }
 
+  // final socketService = Get.find<SocketService>();
   @override
   void onInit() {
+//  Set up the notification listener
+    // socketService.socket!.on('notifications:alert', (data) {
+    //   // Handle the received notification data
+    //   // Display notification using Get.snackbar or other methods
+    //   if (data is Map<String, dynamic>) {
+    //     final notificationData = NotificationData.fromJson(data);
+    //     // final title = data['title'];
+    //     // final summary = data['summary'];
+    //     print(notificationData.summary);
+    //   }
+    // });
+
     // Call fetchUserData here when the HomeController is initialized
     fetchGymPlans();
     fetchUserData();
@@ -81,6 +97,21 @@ class GymPlansController extends BaseController {
   );
 
   final Controller = TextEditingController(
+    text: kDebugMode ? '' : null,
+  );
+  final cardNumberController = TextEditingController(
+    text: kDebugMode ? '' : null,
+  );
+  final cardNameController = TextEditingController(
+    text: kDebugMode ? '' : null,
+  );
+  final cardYearController = TextEditingController(
+    text: kDebugMode ? '' : null,
+  );
+  final cardMonthController = TextEditingController(
+    text: kDebugMode ? '' : null,
+  );
+  final cardCvvController = TextEditingController(
     text: kDebugMode ? '' : null,
   );
 //   final planController = TextEditingController(
@@ -166,9 +197,9 @@ class GymPlansController extends BaseController {
       isGymPlanLoading(false);
       // print(response.body);
       // isLoading.value = false;
+      final jsonResponse = jsonDecode(response.body);
+      String message = jsonResponse['message'].toString();
       if (response.statusCode == 200) {
-        final jsonResponse = jsonDecode(response.body);
-
         final data = jsonResponse['data']['items'];
         thisGymPlans.clear();
         print(data);
@@ -217,11 +248,18 @@ class GymPlansController extends BaseController {
 
         update();
       } else {
+        print(jsonResponse['message']);
         isGymPlanLoading(false);
-        Get
-          ..snackbar('session expired', 'login to continue',
-              backgroundColor: errorLight);
-        Get.offAndToNamed(Routes.gymOwnerLogin);
+        if (message == 'no BEARER authorization header provided') {
+          Get..snackbar('', '$message', backgroundColor: errorLight);
+          Get.offAndToNamed(Routes.login);
+        } else if (message == 'you are not registered with this gym') {
+          Get..snackbar('', '$message', backgroundColor: errorLight);
+          Get.back();
+        } else {
+          Get.offAndToNamed(Routes.login);
+        }
+        //
       }
     } catch (e) {
       Get.snackbar('error', '$e', backgroundColor: errorDark);
@@ -236,9 +274,12 @@ class GymPlansController extends BaseController {
     final username = userDataBox.get('username', defaultValue: '');
     final phoneNumber = userDataBox.get('phone', defaultValue: '');
     final token = userDataBox.get('token', defaultValue: '');
+    final id = userDataBox.get('userId', defaultValue: '');
+    print(id);
     // Update the observable variables
     fullname.value = fullName.toString();
     userName.value = username.toString();
+    iD.value = id.toString();
     this.phoneNumber.value = phoneNumber.toString();
 
     // await userDataBox.close();
@@ -269,5 +310,60 @@ class GymPlansController extends BaseController {
 
     // final jsonResponse = jsonDecode(response.body);
     // return jsonResponse;
+  }
+
+  Future<void> performDebitCardCharge() async {
+    try {
+      // Prepare the request data
+      final requestBody = {
+        "tx_ref": DateFormat('yyyy-MM-dd kk:mm:ss a').format(DateTime.now()),
+        "card_number": cardNumberController.text,
+        "cvv": cardCvvController.text,
+        "expiry_month": cardMonthController.text,
+        "expiry_year":
+            cardYearController.text, // Replace with your transaction reference
+        "amount": 100, // Replace with the amount you want to charge (in kobo)
+        "currency": "NGN", // Replace with the desired currency code
+
+        "redirect_url":
+            "https://riilfit-api.vercel.app/debit-cards/new/?userId=$iD", // Replace with your redirect URL
+
+        "email": "customer@example.com", // Replace with customer's email
+        "fullname": cardNameController.text, // Replace with customer's name
+        // Add other customer details as needed
+
+        // Add other required parameters as needed
+      };
+
+      print(111111);
+      final response = await http.post(
+        Uri.parse('https://api.flutterwave.com/v3/charges?type=card'),
+        headers: {
+          'Authorization':
+              'Bearer FLWPUBK_TEST-dd5ad6338e5b0ba65dcf0ae9481d0792-X', // Replace with your Flutterwave API key
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(requestBody),
+      );
+
+      if (response.statusCode == 200) {
+        // Charge request successful, parse the response
+        final jsonResponse = jsonDecode(response.body);
+        print(jsonResponse);
+        // final chargeResponse = ChargeResponse.fromJson(jsonResponse);
+
+        // Handle the charge response as needed
+        // print(chargeResponse);
+
+        // Redirect the user to the Flutterwave payment page using the chargeResponse.data.link
+        // You can use a WebView or open the link in the user's browser
+      } else {
+        // Handle error response
+        print('Charge request failed: ${response.body}');
+      }
+    } catch (e) {
+      // Handle exceptions
+      print('Error: $e');
+    }
   }
 }
