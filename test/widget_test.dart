e1c4e2392dfe32bd90d159ed3b454410a0af9aa5
ferdinaand <ekpo546@gmail.com>
2023-11-02 13:57:26 +0100

    RillFit/ Update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:riilfit/main.dart';
+import 'package:riilfit/src/utils/config.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(RiilfitApp(routes: ''));
+    await tester.pumpWidget(RiilfitApp(
+      routes: '',
+      socketService: SocketService(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
